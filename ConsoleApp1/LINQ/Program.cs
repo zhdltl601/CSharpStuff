74275cf959465812f2cefce871c60843e24ddfe4
zhdltl601 <zhdltl601@gmail.com>
2024-10-15 00:16:31 +0900

    Added Linq Project

---
+++ ConsoleApp1/LINQ/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Linq;
+namespace LINQ
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
