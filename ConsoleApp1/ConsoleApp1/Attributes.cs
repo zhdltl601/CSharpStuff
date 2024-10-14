545df972c6ea9b3c3f39127fa902372b2192035c
zhdltl601 <zhdltl601@gmail.com>
2024-10-15 00:12:59 +0900

    Revert "Added Reactive Project"
    This reverts commit 9089f0e08d03f36b91272e74c8f35c14cf60ad4f.

--- ConsoleApp1/ConsoleApp1/Attributes.cs
+++ ConsoleApp1/ConsoleApp1/Attributes.cs
@@ -1,17 +1,8 @@
 ﻿using System;
 namespace ConsoleApp1
 {
-    [AttributeUsage(AttributeTargets.All, AllowMultiple = true, Inherited = false)]
-    public class MSDNReferenceAttribute : Attribute
-    {
-        public MSDNReferenceAttribute(string msdnLink)
-        {
-
-        }
-    }
-
     [AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = false)]
-    internal class BAttribute : Attribute
+    class BAttribute : Attribute
     {
         public string Test { get; }
         public BAttribute(string value)
